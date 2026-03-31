5d2b9816ae8702a2f4f97daf33edc9a709fd6479
serens-star <sramafunda@gmail.com>
2026-04-01 01:25:54 +0200

    Money Snapshot Page

---
+++ AbsaNextGen/src/components/sideBar.jsx
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom";
+import "../styles/sideBar.css";
+
+const navPages = [
+  {
+    label: "Money Snapshot",
+    to: "/",
+  },
+];
+export default navPages;
---
+++ AbsaNextGen/src/pages/moneySnapshot.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function moneySnapshot() {
+  return (
+    <div>
+      <h1>Money Snapshot</h1>
+    </div>
+  );
+}
